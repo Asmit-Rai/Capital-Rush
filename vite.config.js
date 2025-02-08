a94e97fd5e45049b180594f911f418e8f788a1e4
Asmit-Rai <211b075@juetguna.co.in>
2025-02-08 06:13:55 +0530

    Run Build

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,19 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss(),],
   build: {
     outDir: "dist",
     rollupOptions: {
-      external: [], // Remove "axios" from external
+      external: [],
+      resolve: {
+        alias: {
+          axios: "axios",
+        },
+      },
     },
   },
-  resolve: {
-    alias: {
-      axios: "axios",
-    },
-  },
-});
+})
